f03d648cea339337bfd5d64ec46c46b3747625ca
Kerollos0Samy0 <kokosamy1111@gmail.com>
2026-07-07 09:36:32 +0300

    fix: Add databaseURL for Europe server

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,8 @@ const firebaseConfig = {
   projectId: "egtma3-l-shabab",
   storageBucket: "egtma3-l-shabab.firebasestorage.app",
   messagingSenderId: "739555351342",
-  appId: "1:739555351342:web:45200268d1d676d59e38bf"
+  appId: "1:739555351342:web:45200268d1d676d59e38bf",
+  databaseURL: "https://egtma3-l-shabab-default-rtdb.europe-west1.firebasedatabase.app"
 };
 
 const app = initializeApp(firebaseConfig);
